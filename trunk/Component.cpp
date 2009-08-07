ba1f296da0e98c6100cb3f72402e2745f016cca4
Karel.Crombecq <Karel.Crombecq@a5989ff4-8365-11de-92f0-6bbadbc12360>
2009-08-07 15:28:39 +0000

    initial commit

---
+++ trunk/Component.cpp
@@ -0,0 +1,260 @@
+
+#include "Component.h"
+#include "ObjectManager.h"
+
+
+using namespace Cistron;
+
+
+#include <sstream>
+#include <iostream>
+
+using std::stringstream;
+using namespace std;
+
+
+// constructor/destructor
+Component::Component(string name) : fOwnerId(-1), fName(name), fDestroyed(false), fTrack(false) {
+	static ComponentId IdCounter = 0;
+	fId = ++IdCounter;
+}
+Component::~Component() {
+}
+
+
+// track activity in the log
+/*void Component::trackMe(track) {
+	fTrack = track;
+}*/
+
+
+// check for validity of the object
+bool Component::isValid() {
+	return fOwnerId >= 0 && fName.size() > 0 && !fDestroyed;
+}
+
+
+
+// get owner
+ObjectId Component::getOwnerId() {
+	return fOwnerId;
+}
+void Component::setOwner(ObjectId id) {
+
+	// set the owner
+	fOwnerId = id;
+
+	// respond to ping requests
+	requestMessage("ping", &Component::processPing);
+}
+void Component::setDestroyed() {
+	fDestroyed = true;
+}
+bool Component::isDestroyed() {
+	return fDestroyed;
+}
+
+
+// get id
+ComponentId Component::getId() {
+	return fId;
+}
+string Component::getName() {
+	return fName;
+}
+
+
+// add another component to this object
+void Component::addLocalComponent(Component *c) {
+	ObjectManager->addComponent(getOwnerId(), c);
+}
+
+
+/**
+ * REQUEST FUNCTIONS
+ */
+
+// message request function
+void Component::requestMessage(string message, MessageFunction f) {
+
+	// construct registered component
+	RegisteredComponent reg;
+	reg.callback = f;
+	reg.required = false;
+	reg.component = this;
+	reg.trackMe = false;
+
+	// construct component request
+	ComponentRequest req;
+	req.type = REQ_MESSAGE;
+	req.name = message;
+
+	// forward to object manager
+	ObjectManager->registerGlobalRequest(req, reg);
+}
+
+// require a component in this object
+void Component::requireComponent(string name, MessageFunction f) {
+
+	// construct registered component
+	RegisteredComponent reg;
+	reg.callback = f;
+	reg.required = true;
+	reg.component = this;
+	reg.trackMe = false;
+
+	// construct component request
+	ComponentRequest req;
+	req.type = REQ_COMPONENT;
+	req.name = name;
+
+	// forward to object manager
+	ObjectManager->registerLocalRequest(req, reg);
+}
+
+// register a component request
+void Component::requestComponent(string name, MessageFunction f, bool local) {
+
+	// construct registered component
+	RegisteredComponent reg;
+	reg.callback = f;
+	reg.required = false;
+	reg.component = this;
+	reg.trackMe = false;
+
+	// construct component request
+	ComponentRequest req;
+	req.type = REQ_COMPONENT;
+	req.name = name;
+
+	// forward to object manager
+	if (local) ObjectManager->registerLocalRequest(req, reg);
+	else ObjectManager->registerGlobalRequest(req, reg);
+}
+
+// request all components of one type
+void Component::requestAllComponents(string name, MessageFunction f) {
+
+	// construct registered component
+	RegisteredComponent reg;
+	reg.callback = f;
+	reg.required = false;
+	reg.component = this;
+	reg.trackMe = false;
+
+	// construct component request
+	ComponentRequest req;
+	req.type = REQ_ALLCOMPONENTS;
+	req.name = name;
+
+	// forward to object manager
+	ObjectManager->registerGlobalRequest(req, reg);
+}
+
+
+
+// get a request id
+RequestId Component::getRequestId(ComponentRequestType type, string name) {
+	return ObjectManager->getRequestId(type, name);
+}
+
+
+
+/**
+ * MESSAGING FUNCTIONS
+ */
+
+// send a message
+void Component::sendMessage(string msg, void *payload) {
+	ObjectManager->sendGlobalMessage(msg, this, payload);
+}
+void Component::sendMessage(RequestId reqId, void *payload) {
+	ObjectManager->sendGlobalMessage(reqId, this, payload);
+}
+void Component::sendLocalMessage(string msg, void *payload) {
+	ObjectManager->sendMessageToObject(msg, this, fOwnerId, payload);
+}
+void Component::sendLocalMessage(RequestId reqId, void *payload) {
+	ObjectManager->sendMessageToObject(reqId, this, fOwnerId, payload);
+}
+void Component::sendLocalMessage(RequestId reqId, Message msg) {
+	ObjectManager->sendMessageToObject(reqId, msg, fOwnerId);
+}
+void Component::sendMessageToObject(ObjectId id, string msg, void *payload) {
+	ObjectManager->sendMessageToObject(msg, this, id, payload);
+}
+void Component::sendMessageToObject(ObjectId id, RequestId reqId, void *payload) {
+	ObjectManager->sendMessageToObject(reqId, this, id, payload);
+}
+void Component::sendMessageToObject(ObjectId id, RequestId reqId, Message msg) {
+	ObjectManager->sendMessageToObject(reqId, msg, id);
+}
+
+
+
+// called when added to an object
+void Component::addedToObject() {
+	// does nothing by default
+}
+
+
+// register a unique name
+bool Component::registerName(string s) {
+	return ObjectManager->registerName(fOwnerId, s);
+}
+ObjectId Component::getObjectId(string name) {
+	return ObjectManager->getObjectId(name);
+}
+
+
+// destroy this component
+void Component::destroy() {
+	ObjectManager->destroyComponent(this);
+}
+
+
+// output
+ostream& operator<<(ostream& s, Component &v) {
+	return s << "Component[" << v.getId() << "][" << v.getName() << "] owned by [" << v.getOwnerId() << "]";
+};
+
+
+// to string
+string Component::toString() {
+	stringstream ss;
+	ss << (*this);
+	return ss.str();
+}
+
+
+/**
+ * PING & LOGGING
+ */
+
+
+// process ping
+void Component::processPing(Message msg) {
+	cout << *this << " PING" << endl;
+}
+
+
+// track a component request
+void Component::trackComponentRequest(string name, bool local) {
+
+	// get request id
+	RequestId reqId = ObjectManager->getRequestId(REQ_COMPONENT, name);
+
+	// start tracking
+	ObjectManager->trackRequest(reqId, local, this);
+}
+
+
+// track a message request
+void Component::trackMessageRequest(string message) {
+
+	// get request id
+	RequestId reqId = ObjectManager->getRequestId(REQ_MESSAGE, message);
+cout << "Tracking message request " << message << endl;
+	// start tracking
+	ObjectManager->trackRequest(reqId, false, this);
+}
---
+++ trunk/Object.cpp
@@ -0,0 +1,151 @@
+
+#include "Object.h"
+#include "ObjectManager.h"
+
+
+using namespace Cistron;
+
+
+#include <iostream>
+using namespace std;
+
+
+
+
+// constructor/destructor
+Object::Object(ObjectId id) : fId(id), fFinalized(false) {
+}
+Object::~Object() {
+
+	// delete all components
+	// deleted by the object manager
+	/*for (map<string, list<Component*> >::iterator it = fComponents.begin(); it != fComponents.end(); ++it) {
+		for (list<Component*>::iterator it2 = it->second.begin(); it2 != it->second.end(); ++it2) {
+			delete (*it2);
+		}
+	}*/
+}
+
+
+// is the object finalized
+bool Object::isFinalized() {
+	return fFinalized;
+}
+
+
+// finalize the object
+void Object::finalize() {
+
+	// we're done
+	fFinalized = true;
+}
+
+
+// add a component
+bool Object::addComponent(Component *comp) {
+
+	// component must be valid
+	if (!comp->isValid()) return false;
+
+	// make sure there's no such component yet
+	//if (fComponents.find(comp->getName()) != fComponents.end()) return false;
+
+	// just add of
+	fComponents[comp->getName()].push_back(comp);
+	return true;
+}
+
+
+// get a component
+list<Component*> Object::getComponents(string name) {
+
+	// make sure there's no such component yet
+	if (fComponents.find(name) == fComponents.end()) return list<Component*>();
+
+	// return normally
+	return fComponents[name];
+}
+
+
+// get all components
+list<Component*> Object::getComponents() {
+
+	// append them all
+	list<Component*> comps;
+	for (hash_map<string, list<Component*> >::iterator it = fComponents.begin(); it != fComponents.end(); ++it) {
+		comps.insert(comps.end(), it->second.begin(), it->second.end());
+	}
+
+	// return normally
+	return comps;
+}
+
+
+// remove a component
+void Object::removeComponent(Component *comp) {
+
+	// look for the right component ant delete it
+	list<Component*> comps;
+	for (list<Component*>::iterator it = comps.begin(); it != comps.end(); ++it) {
+		if ((*it)->getId() == comp->getId()) {
+			comps.erase(it);
+			break;
+		}
+	}
+
+	// now we remove all local requests
+	for (unsigned i = 0; i < fLocalRequests.size(); ++i) {
+		list<RegisteredComponent>& regs = fLocalRequests[i];
+		for (list<RegisteredComponent>::iterator it = regs.begin(); it != regs.end();) {
+			if (it->component->getId() == comp->getId()) {
+				it = regs.erase(it);
+			}
+			else ++it;
+		}
+	}
+}
+
+
+// send a local message
+void Object::sendMessage(RequestId reqId, Message msg) {
+
+	// if there are no registered components, we just skip
+	if (fLocalRequests.size() <= reqId) return;
+
+	// just forward to the appropriate registered components
+	list<RegisteredComponent>& regs = fLocalRequests[reqId];
+	for (list<RegisteredComponent>::iterator it = regs.begin(); it != regs.end(); ++it) {
+		if (it->trackMe) {
+			string name;
+			if (msg.type == MESSAGE) {
+				name = ObjectManager->getRequestById(REQ_MESSAGE, reqId);
+			}
+			else {
+				name = ObjectManager->getRequestById(REQ_COMPONENT, reqId);
+			}
+			cout << *it->component << " received message " << name << " from " << *msg.sender << endl;
+		}
+		it->callback(msg);
+	}
+}
+
+// register a request
+void Object::registerRequest(RequestId reqId, RegisteredComponent reg) {
+
+	// TODO OPTIMIZATION
+
+	// if it doesn't exist yet, create it
+	if (fLocalRequests.size() <= reqId) fLocalRequests.resize(reqId+1);
+
+	fLocalRequests[reqId].push_back(reg);
+}
+
+
+// track a local request
+void Object::trackRequest(RequestId reqId, Component *component) {
+
+	// find in global request list
+	for (list<RegisteredComponent>::iterator it = fLocalRequests[reqId].begin(); it != fLocalRequests[reqId].end(); ++it) {
+		if ((*it).component->getId() == component->getId()) (*it).trackMe = true;
+	}
+}
